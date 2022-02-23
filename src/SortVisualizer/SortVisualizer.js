ba3a3174bda39ae70b50e031166d600701bbf44e
tariq <tariqshariff.ts@gmail.com>
2022-02-23 22:12:39 +0530

    setup react app

---
+++ src/SortVisualizer/SortVisualizer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./SortVisualizer.css";
+
+export class SortVisualizer extends Component {
+  render() {
+    return <div>SortVisualizer</div>;
+  }
+}
+
+export default SortVisualizer;
