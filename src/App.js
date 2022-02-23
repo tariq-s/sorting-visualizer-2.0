84e5920eca2f53d2e1c30a59da4a751db7272299
tariq <tariqshariff.ts@gmail.com>
2022-02-23 23:01:57 +0530

    static bars

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SortVisualizer from "./SortVisualizer/SortVisualizer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <SortVisualizer />
     </div>
   );
--- src/SortVisualizer/SortVisualizer.js
+++ src/SortVisualizer/SortVisualizer.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
 import "./SortVisualizer.css";
 
+const N = 80;
+
+function generateRandomArray(n) {
+  const array = [];
+  for (let i = 0; i < n; i++) {
+    array[i] = Math.floor(Math.random() * 100 + 1);
+  }
+  return array;
+}
+
 export class SortVisualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arr: generateRandomArray(N),
+      n: N,
+    };
+  }
+
   render() {
-    return <div>SortVisualizer</div>;
+    return (
+      <div className="sort-visualizer">
+        {this.state.arr.map((bar, idx) => (
+          <div
+            className="bar"
+            key={idx}
+            style={{
+              width: `calc(${100 / this.state.n}% - 2px)`,
+              height: `${bar}%`,
+            }}
+          ></div>
+        ))}
+      </div>
+    );
   }
 }
 
